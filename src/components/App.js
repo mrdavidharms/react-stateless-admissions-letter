16894b9ef76f4cd045856c9ab5ca2bbf3dd2a7f3
David Harms <davidharms@Davids-MacBook-Pro-2.local>
2016-09-06 16:50:06 -0400

    rough

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,42 @@
-import React from 'react';
-const App = props => <div />;
-export default App;
+import React, { Component }from 'react';
 
+const App = props => {
+
+<div />;
+
+
+
+  return (
+    <div className = "letter-background">
+        <img src="src/components/assets/Hogwarts.png"/> 
+          <h1>HOGWARTS SCHOOL OF WITCHRAFT AND WIZARDRY</h1>
+
+      <h4>Headmaster: Albus Dumbledore</h4>
+      <p>(Order of Merlin, First Class, Grand Sorc., Chf. Warlock, Supreme Mugwump,
+      International Confed. of Wizards)</p>
+
+      <p>Dear Adressee,</p>
+
+      <p>We are pleased to inform you that you have a place at Hogwarts School of
+      Witchcraft and Wizardry. Please find enclosed a list of all necessary books and
+      equipment.</p>
+
+      <p>Term begins on 1 September. We await your owl by no later than 31 July.</p>
+
+      <p>Your sincerely,</p>
+
+      <p>Minerva McGonagall
+      Deputy Headmistress
+      </p>
+    </div>
+  );
+};
+
+
+
+
+
+
+
+
+export default App;
